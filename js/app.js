0a5a7c936a9c749d8a801978ee939d279aa58639
Maxim <38426619+Danymiliano@users.noreply.github.com>
2022-10-20 20:09:40 +0300

    HTML & CSS update

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 // Анимация для кнопок навигации
 
-const navAnimation = setNavAnimation = () => {
-  document.querySelectorAll('.navLink').forEach(navLink => {
-    navLink.addEventListener('mouseover', () => {
-        navLink.style = `
-        transition: all 0.4s;
-        font-size: 40px;
-        background-color: var(--mainColor);
-        color: var(--secondColor);
-        padding-bottom: 5px;`
-    })
+// const navAnimation = setNavAnimation = () => {
+//   document.querySelectorAll('.navLink').forEach(navLink => {
+//     navLink.addEventListener('mouseover', () => {
+//         navLink.style = `
+//         transition: all 0.4s;
+//         font-size: 40px;
+//         background-color: var(--mainColor);
+//         color: var(--secondColor);
+//         padding-bottom: 5px;`
+//     })
 
-    navLink.addEventListener('mouseout', () => {
-        navLink.style = `
-        transition: all 0.4s;
-        font-size: 26px;
-        `
-    })
-  }) 
-}
-setNavAnimation()
+//     navLink.addEventListener('mouseout', () => {
+//         navLink.style = `
+//         transition: all 0.4s;
+//         font-size: 26px;
+//         `
+//     })
+//   }) 
+// }
+// setNavAnimation()
 
 // Анимация для кнопки
 
