16c14740a9b045a695a16aa63f21c044d134ae00
Maxim <38426619+Danymiliano@users.noreply.github.com>
2022-11-02 23:23:47 +0300

    Code refactoring

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan')
 const mongoose = require('mongoose');
+require('dotenv').config()
 const methodOverride = require('method-override')
 const setPath = require('./helpers/set-path')
 const postRoutes = require('./routes/post-routes')
@@ -12,8 +13,7 @@ app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 3000;
 const HOST = 'localhost';
-const db = `mongodb+srv://Danymiliano:${token}@VFC-Urozhay.5tl0qwi.mongodb.net/posts?retryWrites=true&w=majority`;
-
+const db = process.env.MONGO_URL
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((res) => console.log('Установили соединение с базой данных'))
@@ -38,31 +38,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'));
 
 app.use(postRoutes);
-
-app.get('/', (req, res) => {
-    const title = 'Home'
-    res.render(setPath('index'), { title })
-})
-
-app.get('/index', (req, res) => {
-    const title = 'Home'
-    res.render(setPath('index'), { title })
-})
-
-app.get('/about', (req, res) => {
-    const title = 'About'
-    res.render(setPath('about'), { title })
-})
-
-app.get('/add-post', (req, res) => {
-    const title = 'Add new post'
-    res.render(setPath('add-post'), { title })
-})
-
-app.get('/squad', (req, res) => {
-    const title = 'Squad'
-    res.render(setPath('squad'), { title })
-})
 
 app.use((req, res) => {
     const title = '404'
